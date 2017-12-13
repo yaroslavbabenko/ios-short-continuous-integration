39b42e79c1de435d91968b332cddc2887929989a
Yaroslav Babenko <>
2017-12-13 13:15:52 +0300

    Added toRoman unit test

--- RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
+++ RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import RomanNumeralCalculator
 
 class RomanNumeralCalculatorTests: XCTestCase {
     
@@ -20,16 +21,10 @@ class RomanNumeralCalculatorTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testToRoman() {
+        XCTAssertEqual(toRoman(1), "I")
+        XCTAssertEqual(toRoman(2), "II")
+        XCTAssertEqual(toRoman(4), "IV")
     }
     
 }
